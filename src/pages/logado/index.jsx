3b8d93c57c48a81a583523a2aef5834f15357420
Esmyrna <Esmyrnaoliveira13@gmail.com>
2023-02-16 13:11:00 -0300

    salvar alterações

--- src/pages/logado/index.jsx
+++ src/pages/logado/index.jsx
@@ -1,20 +1,69 @@
 import Navbar from '../../components/Home/navbar';
 import Carrossel from './carrossel';
+import { AiOutlineSearch } from 'react-icons/ai'
+import foto from '../../Imagens/foto-if.png'
 import './style.scss'
 
-function Logado(){
-    return(
+function Logado() {
+    return (
         <>
-        <Navbar />
-        <div className="bloco-container">
-            <div className="sub-div1">
-                <h2>Últimas conexões</h2>
-                <Carrossel />
+            <Navbar />
+            <div className="bloco-container">
+                <div className="sub-div1">
+                    <h2>Últimas conexões</h2>
+                    <Carrossel />
+                </div>
+                <div className="sub-div2">
+                    <h2>Pesquisa</h2>
+                    <form>
+                        <input type="text" />
+                        <button class="submit-lente" type="submit">
+                             <AiOutlineSearch />
+                        </button>
+                    </form>
+                    <div className="sessao-1">
+                        <div className="lista">
+                            <ul>
+                                <li>
+                                    <div className="info-iniciativas">
+                                        <div className="imagem">
+                                            <img src={foto} />
+                                        </div>
+                                        <h2>Title</h2>
+                                        <h4>Setor</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ratione earum. Magnam perferendis, ea cupiditate quod libero dolor reprehenderit.</p>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div className="info-iniciativas">
+                                        <div className="imagem">
+                                            <img src={foto} />
+                                        </div>
+                                        <h2>Title</h2>
+                                        <h4>Setor</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ratione earum. Magnam perferendis, ea cupiditate quod libero dolor reprehenderit.</p>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div className="info-iniciativas">
+                                        <div className="imagem">
+                                            <img src={foto} />
+                                        </div>
+                                        <h2>Title</h2>
+                                        <h4>Setor</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ratione earum. Magnam perferendis, ea cupiditate quod libero dolor reprehenderit.</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="mapa">
+
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="sub-div2">
-            <h2>Pesquisa</h2>
-            </div>
-        </div>
         </>
     )
 }
